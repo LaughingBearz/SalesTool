cb89d7726780b6affd94a6939dc319d8f2577c04
travi <travi@10.40.42.160>
2017-04-03 19:24:13 -0400

    incomplete Sales stuff

--- GitHubHW/src/gitTutorial/SalesData.java
+++ GitHubHW/src/gitTutorial/SalesData.java
@@ -5,8 +5,10 @@ public class SalesData {
 
    public void display() {
        System.out.println("TODO: Implement data display");
-   } //End display
+       for(int i = 0; i < data.length; i++){
+    	   System.out.println("");
+       }
 
 }  //End class SalesData
-
+}
   
