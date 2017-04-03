b393b486ef72cf7224764359d6d2d79db7f4c2e5
travi <travi@10.40.42.160>
2017-04-03 19:31:26 -0400

    Completed data in SalesData

--- GitHubHW/src/gitTutorial/SalesData.java
+++ GitHubHW/src/gitTutorial/SalesData.java
@@ -4,9 +4,11 @@ public class SalesData {
    int data[] = {0, 4, 42};
 
    public void display() {
-       System.out.println("TODO: Implement data display");
+	   
+	   System.out.println("Data: ");
+	   System.out.println("TODO: Implement data display");
        for(int i = 0; i < data.length; i++){
-    	   System.out.println("");
+    	   System.out.println("Next Value: " + data[1]);
        }
 
 }  //End class SalesData
